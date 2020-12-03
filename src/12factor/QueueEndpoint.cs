132668e6e72004e6d7dcaa27ac4caacfdaaae070
glennc <glennc@microsoft.com>
2020-12-03 13:47:31 -0800

    abstractions.

---
+++ src/12factor/QueueEndpoint.cs
@@ -0,0 +1,36 @@
+
+
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace _12factor
+{
+    public class QueueEndpoint : IMessageEndpoint
+    {
+        private IConfiguration _configuration;
+
+        public QueueEndpoint(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendMessage(string message)
+        {
+            var client = new QueueClient(_configuration["StorageConnectionString"], "12factorqueue");
+            await client.CreateIfNotExistsAsync();
+
+            if (await client.ExistsAsync())
+            {
+                // Send a message to the queue
+                await client.SendMessageAsync(message);
+            }
+        }
+    }
+
+    public interface IMessageEndpoint
+    {
+        Task SendMessage(string message);
+    }
+}
